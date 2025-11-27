bbcf80b8e6e77626086494f82ecc927898f58c72
EmbroiderSnow <1497411439@qq.com>
2025-11-27 20:35:13 +0800

    Add QuickSearch to layout

--- frontend/src/components/layout/AppShell.tsx
+++ frontend/src/components/layout/AppShell.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { AppSidebar } from './AppSidebar'
+import { QuickSearch } from '@/components/search/QuickSearch'
 
 interface AppShellProps {
   children: React.ReactNode
@@ -11,6 +12,10 @@ export function AppShell({ children }: AppShellProps) {
     <div className="flex h-screen overflow-hidden">
       <AppSidebar />
       <div className="flex-1 flex flex-col min-h-0">
+        <header className="h-16 flex items-center justify-end px-4 border-b bg-background shrink-0 z-10">
+          <div className='flex-1'/>
+          <QuickSearch />
+        </header>
         <main className="flex-1 flex flex-col min-h-0 overflow-hidden">
           {children}
         </main>
